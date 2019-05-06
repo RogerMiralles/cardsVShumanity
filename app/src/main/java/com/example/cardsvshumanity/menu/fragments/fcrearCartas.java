194ca7677d8b090889137022eae2aa616b5d92ee
RogerMiralles <miralles.monfort@gmail.com>
2019-05-06 10:45:42 +0200

    todo

---
+++ app/src/main/java/com/example/cardsvshumanity/menu/fragments/fcrearCartas.java
@@ -0,0 +1,30 @@
+package com.example.cardsvshumanity.menu.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cardsvshumanity.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class fcrearCartas extends Fragment {
+
+
+    public fcrearCartas() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fcrear_cartas, container, false);
+    }
+
+}
