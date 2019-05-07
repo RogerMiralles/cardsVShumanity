03e7c2320d3ab538b1aa69547896b90d79c083d5
RogerMiralles <miralles.monfort@gmail.com>
2019-05-07 14:24:27 +0200

    Login funcional falta poner excepciones en el login

--- app/src/main/java/com/example/cardsvshumanity/logReg/login.java
+++ app/src/main/java/com/example/cardsvshumanity/logReg/login.java
@@ -1,24 +1,51 @@
 package com.example.cardsvshumanity.logReg;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.cardsvshumanity.MainActivity;
 import com.example.cardsvshumanity.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private EditText correo;
+    private EditText contra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        correo=findViewById(R.id.eTxtCorreo);
+        contra=findViewById(R.id.eTxtPass);
+        mAuth=FirebaseAuth.getInstance();
     }
 
     public void onClickLogear(View view) {
-        Intent listSong = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(listSong);
+        if(correo.getText()!=null||contra.getText()!=null){
+            mAuth.signInWithEmailAndPassword(correo.getText().toString(),contra.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        finish();
+                    }else{
+                        Toast.makeText(login.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }else{
+            Toast.makeText(this,"campos vacios",Toast.LENGTH_LONG).show();
+        }
+
     }
 
     public void onClickRegistrar(View view) {
