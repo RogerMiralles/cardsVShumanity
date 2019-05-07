a66ff23db7efa85da8136153cf9d6620c3ddfacf
Antonio <toni.cano.redondo@gmail.com>
2019-05-07 13:16:15 +0200

    Commit Basura

--- app/src/main/java/com/example/cardsvshumanity/MainActivity.java
+++ app/src/main/java/com/example/cardsvshumanity/MainActivity.java
@@ -16,7 +16,6 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
@@ -31,74 +30,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
-        GoogleSignInAccount acc = GoogleSignIn.getLastSignedInAccount(this);
-        if(acc != null){
-            new AlertDialog.Builder(MainActivity.this).setMessage(acc.getEmail())
-                    .setNeutralButton(android.R.string.ok, null).show();
-        }
-        else {
-            signInSilently();
-        }
-    }
 
-    private void signInSilently() {
-        final GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN)
-                .requestServerAuthCode(getString(R.string.default_web_client_id)).build();
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if (GoogleSignIn.hasPermissions(account, signInOptions.getScopeArray())) {
-            // Already signed in.
-            // The signed in account is stored in the 'account' variable.
-            GoogleSignInAccount signedInAccount = account;
-            Log.d(MainActivity.class.getSimpleName(), signedInAccount.getDisplayName());
-
-        } else {
-            // Haven't been signed-in before. Try the silent sign-in first.
-            GoogleSignInClient signInClient = GoogleSignIn.getClient(this, signInOptions);
-            signInClient
-                    .silentSignIn()
-                    .addOnCompleteListener(
-                            this,
-                            new OnCompleteListener<GoogleSignInAccount>() {
-                                @Override
-                                public void onComplete(@NonNull Task<GoogleSignInAccount> task) {
-                                    try{
-                                        // The signed in account is stored in the task's result.
-                                        GoogleSignInAccount signedInAccount = task.getResult(ApiException.class);
-                                        Log.d(MainActivity.class.getSimpleName(), signedInAccount.getDisplayName());
-                                    } catch(ApiException e) {
-                                        // Player will need to sign-in explicitly using via UI.
-                                        // See [sign-in best practices](http://developers.google.com/games/services/checklist) for guidance on how and when to implement Interactive Sign-in,
-                                        // and [Performing Interactive Sign-in](http://developers.google.com/games/services/android/signin#performing_interactive_sign-in) for details on how to implement
-                                        // Interactive Sign-in.
-                                        Log.d(MainActivity.class.getSimpleName(), e.toString());
-                                        SignInInteractive(signInOptions);
-                                    }
-                                }
-                            });
-        }
-    }
-
-    private void SignInInteractive(GoogleSignInOptions googleSignInOptions){
-            GoogleSignInClient client = GoogleSignIn.getClient(MainActivity.this, googleSignInOptions);
-            startActivityForResult(client.getSignInIntent(), RC_SIGN_IN);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            if (result.isSuccess()) {
-                // The signed in account is stored in the result.
-                GoogleSignInAccount signedInAccount = result.getSignInAccount();
-            } else {
-                String message = result.getStatus().getStatusMessage();
-                if (message == null || message.isEmpty()) {
-                    message = "Error al conectar con Google Play Games";
-                }
-                new AlertDialog.Builder(this).setMessage(message)
-                        .setNeutralButton(android.R.string.ok, null).show();
-            }
-        }
     }
 }
