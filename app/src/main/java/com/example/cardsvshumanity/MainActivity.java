903169a571ad812dbe76f95eba273709e41d548e
RogerMiralles <miralles.monfort@gmail.com>
2019-05-08 13:22:24 +0200

    idioma canvia imagen

--- app/src/main/java/com/example/cardsvshumanity/MainActivity.java
+++ app/src/main/java/com/example/cardsvshumanity/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.cardsvshumanity.jugarPerfil.perfil;
@@ -20,6 +22,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 import java.util.Locale;
+import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,14 +32,21 @@ public class MainActivity extends AppCompatActivity {
     private Button mPerfil;
     private Button mSalir;
     private final int codigo=0;
+    private ImageButton mIbtn;
+    private TextView txt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.setTitle("Cartas Contra la Humanidad");
+        //loadLocale();
+        this.setTitle("CCH");
         mJuegaIniciaSes = findViewById(R.id.btnJugar);
         mPerfil=findViewById(R.id.btnPerfil);
         mSalir=findViewById(R.id.btnSalida);
+        mIbtn=findViewById(R.id.imBtnIdioma);
+        txt=findViewById(R.id.txtTitulo);
+        loadLocale();
         mAuth = FirebaseAuth.getInstance();
         ponerCosasVisIn();
     }
@@ -75,6 +85,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ponerCosasVisIn(){
+        txt.setText(getString(R.string.cartas_contra_la_humanidad));
         mUser=mAuth.getCurrentUser();
         if(mUser == null){
             mJuegaIniciaSes.setText(getString(R.string.inicia_session));
@@ -88,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+
     private void showChangeLanguageDialog() {
         final String[] listItems={"Castellano","Catalan","English"};
         AlertDialog.Builder mBuilder=new AlertDialog.Builder(this);
@@ -98,7 +110,6 @@ public class MainActivity extends AppCompatActivity {
                 if(i==0){
                     Toast.makeText(getApplicationContext(), "Traduciendo al castellano", Toast.LENGTH_SHORT).show();
                     setLocale("es");
-
                     recreate();
                 }
                 else if(i==1){
@@ -132,6 +143,17 @@ public class MainActivity extends AppCompatActivity {
     public void loadLocale(){
         SharedPreferences prefs =getSharedPreferences("Settings", Activity.MODE_PRIVATE);
         String language=prefs.getString("My_Lang","");
+        switch(language){
+            case "ca":
+                mIbtn.setImageResource(R.drawable.catalonia);
+                break;
+            case "es":
+                mIbtn.setImageResource(R.drawable.spain);
+                break;
+            case "en":
+                mIbtn.setImageResource(R.drawable.uk);
+                break;
+        }
         setLocale(language);
     }
 }
