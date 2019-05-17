9c56c7cce59b9f544d55fdddd54ff60dbf5b55df
Antonio <toni.cano.redondo@gmail.com>
2019-05-17 10:40:01 +0200

    Merge

--- app/src/main/java/com/example/cardsvshumanity/logReg/registre.java
+++ app/src/main/java/com/example/cardsvshumanity/logReg/registre.java
@@ -13,6 +13,7 @@ import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.PersistableBundle;
 import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
